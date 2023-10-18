9ed9152d994961ce1c6162ea38b5d8f5f6973026
MarioSantoro <mariosantoro124@gmail.com>
2023-10-18 11:32:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+import SideBar from './components/SideBar.vue';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<template>
+  <SideBar />
+</template>
+
+<style lang="scss">
+@import url('../node_modules/bootstrap/scss/bootstrap.scss');
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav></nav>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+nav {
+    background-color: #2A4054;
+    width: 20%;
+}
+</style>
